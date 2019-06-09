e6907118f0f799c949b7fba4d1c47fc1a244a6a2
ranjeetgithub2019 <50949517+ranjeetgithub2019@users.noreply.github.com>
2019-06-09 20:31:20 +0530

    Add files via upload

---
+++ StaticMethod.java
@@ -0,0 +1,27 @@
+package pack;
+
+public class StaticMethod {
+	
+	static int x=10,y=19;
+	int z=8;
+	
+	static int sum()
+	{
+	
+		return x=y+z;
+		
+	}
+
+	public static void main(String[] args) {
+		
+		
+		
+		int z=sum();
+		System.out.println(z);
+		
+		
+		
+		
+	}
+
+}
